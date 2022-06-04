68ffb697bf78a1ca47d834e315dc0f6a8d5ba9da
Lutfi Aldri Permana <83573252+lutfialdrii@users.noreply.github.com>
2022-06-04 18:22:14 +0700

    Update all.php

--- all.php
+++ all.php
@@ -5,12 +5,13 @@ include "./connection.php";
  * @var $connection PDO
  */
 
+//query
  $query = "SELECT * FROM buku";
     $stmt = $connection->query($query);
     $stmt->setFetchMode(PDO::FETCH_ASSOC);
 
     $result = $stmt->fetchAll();
-//query
+//Menampilkan dalam bentuk JSON
 header('Content-Type: application/json');
 echo json_encode($result);
 ?>
